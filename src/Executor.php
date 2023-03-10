4169b1e440d9b17f71217a80d1d6487b22c78381
Don Hardman <don.hardman@muvon.io>
2023-03-10 04:39:30 +0000

    Initial release

---
+++ src/Executor.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+/*
+ Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2 or any later
+ version. You should have received a copy of the GPL license along with this
+ program; if you did not, you can find it at http://www.gnu.org/
+ */
+
+namespace Manticoresearch\Buddy\Plugin\Test;
+
+use Manticoresearch\Buddy\Core\ManticoreSearch\Client as HTTPClient;
+use Manticoresearch\Buddy\Core\Plugin\Executor as BaseExecutor;
+use Manticoresearch\Buddy\Core\Task\Task;
+use Manticoresearch\Buddy\Core\Task\TaskResult;
+use RuntimeException;
+use parallel\Runtime;
+
+/**
+ * This is the parent class to handle erroneous Manticore queries
+ */
+final class Executor extends BaseExecutor {
+
+	/** @var HTTPClient $manticoreClient */
+	protected HTTPClient $manticoreClient;
+
+	/**
+	 *  Initialize the executor
+	 *
+	 * @param Request $request
+	 * @return void
+	 */
+	public function __construct(public Request $request) {
+	}
+
+	/**
+	 * Process the request and return self for chaining
+	 *
+	 * @return Task
+	 * @throws RuntimeException
+	 */
+	public function run(Runtime $runtime): Task {
+
+		$taskFn = static function (int $timeout): TaskResult {
+			sleep($timeout);
+			return new TaskResult([[]]);
+		};
+
+		$createMethod = $this->request->isDeferred ? 'deferInRuntime' : 'createInRuntime';
+		return Task::$createMethod($runtime, $taskFn, [$this->request->timeout])->run();
+	}
+
+	/**
+	 * @return array<string>
+	 */
+	public function getProps(): array {
+		return [];
+	}
+}
---
+++ src/Request.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Plugin\Test;
+
+use Manticoresearch\Buddy\Core\Network\Request as NetworkRequest;
+use Manticoresearch\Buddy\Core\Plugin\Request as BaseRequest;
+
+/**
+ * Request for Backup command that has parsed parameters from SQL
+ */
+final class Request extends BaseRequest {
+
+	public function __construct(public int $timeout = 0, public bool $isDeferred = false) {
+	}
+
+	/**
+	 * @param NetworkRequest $request
+	 * @return static
+	 */
+	public static function fromNetworkRequest(NetworkRequest $request): static {
+		// Request for Test command emulating hung Buddy requests
+		// Contains info on request timeout and the type of the command's Task(deferred or not)
+		// E.g.: test 6/deferred ; test 10 ; test deferred
+		$self = new static();
+		$matches = [];
+		preg_match('/^\s*test\s+(\d+)?\/?(deferred)?\s*$/i', $request->payload, $matches);
+		$self->timeout = isset($matches[1]) ? abs((int)$matches[1]) : 0;
+		$self->isDeferred = isset($matches[2]);
+
+		return $self;
+	}
+
+	/**
+	 * @param NetworkRequest $request
+	 * @return bool
+	 */
+	public static function hasMatch(NetworkRequest $request): bool {
+		return stripos($request->payload, 'test') === 0;
+	}
+}
